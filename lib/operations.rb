ef5a340225f027dd2a044bae901dbddc37de924f
Kachel Wittig <kjwittig@gmail.com>
2018-02-25 04:25:44 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,19 @@
 def unsafe?(speed)
 
+	if speed < 40 || speed > 60
+			return true
+		else 
+			return false
+	end
+	
 end
 
 
 
 def not_safe?(speed)
+
+# conditional ? action_if_true : action_if_false
+	speed ? speed < 40 || speed > 60 return true : false
 	
 end
 	
